ae1ce2c20d9b61485c9c942816e4d8f2ccb31807
Jar7011 <jareyes1324@gmail.com>
2024-09-11 22:01:36 -0600

    Tidied up data and created linear regression model, made predictions, created kaggle submission file rightly formatted

--- Bike.R
+++ Bike.R
@@ -1,15 +1,45 @@
+# Load necessary libraries
 library(tidyverse)
 library(tidymodels)
 library(DataExplorer)
 library(vroom)
 library(patchwork)
 
+# Read in training data
 bike_training_data <- vroom('train.csv')
+
+# Clean tidy up training data
+bike_training_data <- bike_training_data |>
+  select(-casual, -registered)
+bike_training_data$season <- factor(bike_training_data$season,
+                                    levels = c(1,2,3,4),
+                                    labels = c('Spring', 'Summer', 
+                                               'Fall', 'Winter'))
+bike_training_data$weather <- factor(bike_training_data$weather,
+                                     levels = c(1,2,3,4))
+bike_training_data <- bike_training_data |>
+  mutate(weather = ifelse(weather == 4, 3, weather))
+
+# Read in test data
+bike_test_data <- vroom('test.csv')
+
+# Clean and tidy up test data to match training data format
+bike_test_data$season <- factor(bike_test_data$season,
+                                    levels = c(1,2,3,4),
+                                    labels = c('Spring', 'Summer', 
+                                               'Fall', 'Winter'))
+bike_test_data$weather <- factor(bike_test_data$weather,
+                                     levels = c(1,2,3,4))
+bike_test_data <- bike_test_data |>
+  mutate(weather = ifelse(weather == 4, 3, weather))
+
+# Some initial EDA plots
 glimpse(bike_training_data)
 plot_intro(bike_training_data)
 plot_correlation(bike_training_data)
 plot_missing(bike_training_data)
 
+# Create four other EDA plots
 weather_barplot <- ggplot(bike_training_data, aes(x=weather)) +
   geom_bar() +
   labs(x = 'Weather Condition',
@@ -38,11 +68,26 @@ temp_histogram <- ggplot(bike_training_data, aes(x=humidity)) +
        title = 'Temperature Distribution') +
   theme_minimal()
 
+# Put the four plots together
 (weather_barplot + working_day_plot) / (temp_atemp_corr + temp_histogram)
 
-
 # Set up linear regression model
-# linear_model <- linear_reg() |>
-#   set_engine('lm') |>
-#   set_mode('regression') |>
-#   fit(formula)
+linear_model <- linear_reg() |>
+  set_engine('lm') |>
+  set_mode('regression') |>
+  fit(formula=log(count)~., data=bike_training_data)
+
+# Make predictions
+lm_predictions <- exp(predict(linear_model,
+                              new_data = bike_test_data))
+
+## Format the Predictions for Submission to Kaggle
+kaggle_submission <- lm_predictions %>%
+  bind_cols(., bike_test_data) %>% # Bind predictions with test data
+  select(datetime, .pred) %>% # Just keep datetime and prediction variables
+  rename(count = .pred) %>% # Rename to count (for submission to Kaggle)
+  mutate(count = pmax(0, count)) %>% # Pointwise max of (0, prediction)
+  mutate(datetime = as.character(format(datetime))) # Needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x = kaggle_submission, file = "./Linear_Predictions.csv", delim = ",")
