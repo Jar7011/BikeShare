08f4ae2845b99d70a398d8b1c7a2916d23a32603
Jar7011 <jareyes1324@gmail.com>
2024-09-18 01:39:59 -0600

    Implemented linear regression model using recipe and workflow

--- Bike.R
+++ Bike.R
@@ -9,16 +9,13 @@ library(poissonreg)
 # Read in training data
 bike_training_data <- vroom('train.csv')
 
+# Read in test data
+bike_test_data <- vroom('test.csv')
+
 ## Cleaning and organizing data ##
 # Clean and tidy up training data
 bike_training_data <- bike_training_data |>
   select(-casual, -registered)
-# Create recipe
-model_recipe <- recipe(count~., data = bike_training_data) |>
-  step_mutate(weather=factor(weather, levels=c(1,2,3,4))) |>
-  
-
-
 
 bike_training_data$season <- factor(bike_training_data$season,
                                     levels = c(1,2,3,4),
@@ -91,7 +88,7 @@ linear_model <- linear_reg() |>
 lm_predictions <- exp(predict(linear_model,
                               new_data = bike_test_data))
 
-## Format the Predictions for Submission to Kaggle
+# Format the Predictions for Submission to Kaggle
 kaggle_submission <- lm_predictions %>%
   bind_cols(., bike_test_data) %>% # Bind predictions with test data
   select(datetime, .pred) %>% # Just keep datetime and prediction variables
@@ -99,7 +96,7 @@ kaggle_submission <- lm_predictions %>%
   mutate(count = pmax(0, count)) %>% # Pointwise max of (0, prediction)
   mutate(datetime = as.character(format(datetime))) # Needed for right format to Kaggle
 
-## Write out the file
+# Write out the file
 vroom_write(x = kaggle_submission, file = "./Linear_Predictions.csv", delim = ",")
 
 ## Poisson Regression ##
@@ -113,7 +110,7 @@ poisson_model <- poisson_reg() |>
 pois_predictions <- predict(poisson_model,
                             new_data = bike_test_data)
 
-## Format the Predictions for Submission to Kaggle
+# Format the Predictions for Submission to Kaggle
 pois_kaggle_submission <- pois_predictions %>%
   bind_cols(., bike_test_data) %>% # Bind predictions with test data
   select(datetime, .pred) %>% # Just keep datetime and prediction variables
@@ -122,3 +119,54 @@ pois_kaggle_submission <- pois_predictions %>%
 
 # Write out the file
 vroom_write(x = pois_kaggle_submission, file = "./Poisson_Predictions.csv", delim = ",")
+
+
+## Linear model with recipe and workflow ##
+# Read in training data again to reset file
+bike_training_data_2 <- vroom('train.csv')
+
+# Read in test data again to reset file
+bike_test_data_2 <- vroom('test.csv')
+
+# Eliminate casual and registered columns from training data and 
+#change count to log(count)
+bike_training_data_2 <- bike_training_data_2 |>
+  select(-casual, -registered) |>
+  mutate(count = log(count))
+
+# Create recipe
+bike_recipe <- recipe(count ~ ., data=bike_training_data_2) |>
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) |>
+  step_mutate(weather = factor(weather, levels = c(1,2,3,4))) |>
+  step_time(datetime, features = 'hour') |>
+  step_mutate(season = factor(season,
+                              levels = c(1,2,3,4),
+                              labels = c('Spring', 'Summer', 
+                                         'Fall', 'Winter'))) |>
+  step_rm(atemp)
+
+# Define model
+lin_model <- linear_reg() |>
+  set_engine('lm') |>
+  set_mode('regression')
+
+# Combine into workflow
+bike_workflow <- workflow() |>
+  add_recipe(bike_recipe) |>
+  add_model(lin_model) |>
+  fit(data = bike_training_data_2)
+
+# Get predictions
+lin_preds <- predict(bike_workflow, new_data = bike_test_data_2) |>
+  mutate(.pred = exp(.pred))
+
+# Format the Predictions for Submission to Kaggle
+kaggle_submission <- lin_preds %>%
+  bind_cols(., bike_test_data_2) %>% # Bind predictions with test data
+  select(datetime, .pred) %>% # Just keep datetime and prediction variables
+  rename(count = .pred) %>% # Rename to count (for submission to Kaggle)
+  mutate(count = pmax(0, count)) %>% # Pointwise max of (0, prediction)
+  mutate(datetime = as.character(format(datetime))) # Needed for right format to Kaggle
+
+# Write out the file
+vroom_write(x = kaggle_submission, file = "./Linear_Predictions_Workflow.csv", delim = ",")
